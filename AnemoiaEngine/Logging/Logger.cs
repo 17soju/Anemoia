eccd29fd24e05e676082b2beb2f94ed22095b0bd
flats <hiimhibiscus@gmail.com>
2023-01-04 11:04:03 -0500

    logging start

--- AnemoiaEngine/Logging/Logger.cs
+++ AnemoiaEngine/Logging/Logger.cs
@@ -10,5 +10,42 @@ namespace Anemoia.AnemoiaEngine.Logging
     public abstract class Logger : IDisposable
     {
 
+        /// <summary>
+        /// path location
+        /// </summary>
+        private readonly string path;
+
+        private FileStream stream;
+        private StreamWriter writer;
+        private string time = DateTime.Now.ToLongTimeString();
+
+        public Logger(string logPath)
+        {
+            if (string.IsNullOrEmpty(path))
+            {
+                throw new ArgumentNullException($"path shouldnt be null/empty");
+            }
+
+            path = logPath;
+        }
+
+        public void Log()
+        {
+            if (!File.Exists(path))
+            {
+                return;
+            }
+            writer.WriteLine(time);
+        }
+
+        public void Dispose()
+        {
+            writer.Dispose();
+            stream.Dispose();
+        }
+        
+
+
+
     }
 }
